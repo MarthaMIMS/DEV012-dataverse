6c14039a5114aee89a4780d3f7a013d9e427d097
MarthaMIMS <marthameliton1903@gmail.com>
2023-10-04 17:33:58 -0600

    5041023_imagenes

--- src/view.js
+++ src/view.js
@@ -1,23 +1,28 @@
 export const renderItems = (data) => {
   console.log(data)
   // Aquí comienza tu código y puedes retornar lo que tu necesites
-  const person1= document.getElementById('root');
 
+  const person1 = document.getElementById('root');
+const ul =document.createElement('ul');
+data.forEach(item =>{
+const lista=document.createElement('li');
+//lista  
+}
 
- data.forEach(item => {
-  const nombres=document.createElement('dl');
-  nombres.setAttribute('itemscope','');
-  nombres.setAttribute('itemtype','HomeroSimpson');
-  nombres.textContent=item.name;
-  person1.appendChild(nombres);
+)
 
-  /*const anio=document.createElement('dt')
-  anio.textContent="Año de Nacimiento";
-  const descripcion=document.createElement('dd')
-  descripcion.textContent=item.description;
-  person1.append(nombres,anio,descripcion);*/
-
-});
+  data.forEach(item => {
+    const nombres = document.createElement('dl');
+    nombres.setAttribute('itemscope', '');
+    nombres.setAttribute('itemtype', 'Personaje de los Simpson');
+    nombres.textContent = item.name;
+    person1.appendChild(nombres);
+    const imagen = document.createElement('img');
+    imagen.setAttribute('src', '');
+    imagen.setAttribute('alt', 'Personaje de los Simpson');
+    imagen.textContent = item.imageUrl;
+    person1.appendChild(imagen);
+  });
 
   return 'example';
 };
