23b23b898b8f4a61d26a66d4e064012c3adeaeb8
DanielaBustamante <bustamantegd@outlook.com>
2023-10-10 22:37:38 -0600

    filtro_sort

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -2,30 +2,29 @@ export function filterData(data, filterBy) {
   const familiasimpson = data.filter(filtro1 => filtro1.lastname === filterBy);
   return familiasimpson;
 
-};
-function filterSort(data, sortBy, sortOrder) {
-  // Checamos que sortOrder sea "asc" o "desc"
-  if (sortOrder !== "asc" && sortOrder !== "desc") {
-
-  }
-
-  // Utilizamos un bucle for para aplicar la ordenación en función de sortBy y sortOrder
-  for (let i = 0; i < data.length; i++) {
-    for (let j = i + 1; j < data.length; j++) {
-      const itemA = data[i];
-      const itemB = data[j];
-      const valueA = itemA[sortBy];
-      const valueB = itemB[sortBy];
-
-      if ((sortOrder === "asc" && valueA > valueB) || (sortOrder === "desc" && valueA < valueB)) {
-        // Intercambiar elementos si sortOrder es "asc" y valueA es mayor o
-        // si sortOrder es "desc" y valueA es menor
-        data[i] = itemB;
-        data[j] = itemA;
-      }
-    }
-  }
-
-  return data;
 }
+
+export function sortData(data, sortBy, sortOrder) {
+  // Crear una copia de los datos originales
+  const sortedData = data.slice();
+
+  if (sortBy) { // Verifica si se ha seleccionado un campo de ordenamiento
+    sortedData.sort((a, b) => {
+      const nameA = a[sortBy].toLowerCase();
+      const nameB = b[sortBy].toLowerCase();
+
+      if (sortOrder === "asc") {
+        return nameA.localeCompare(nameB);
+      } else if (sortOrder === "desc") {
+        return nameB.localeCompare(nameA);
+      }
+
+      // Si sortOrder no es "asc" ni "desc", no se aplica ordenamiento
+      return 0;
+    });
+  }
+
+  return sortedData;
+}
+
 
--- src/main.js
+++ src/main.js
@@ -1,32 +1,47 @@
-import { filterData} from './dataFunctions.js';
+import { filterData } from './dataFunctions.js';
 import { renderItems } from './view.js';
+import { sortData } from './dataFunctions.js';
 import data from './data/dataset.js';
 console.log(filterData, renderItems(data), data);
 const boton = document.getElementById('limpiar');
 const limpiofiltro1 = document.getElementById('select-filter');
 const limpiofiltro2 = document.getElementById('select-sort');
 boton.addEventListener('click', function () {
-    limpiofiltro1.selectedIndex = 0;
-    limpiofiltro2.selectedIndex = 0;
+  limpiofiltro1.selectedIndex = 0;
+  limpiofiltro2.selectedIndex = 0;
+  renderItems(data);
+});
+
+const filtroClave = document.querySelector('select[data-testid="select-filter"]');//nos da el valor seleccionado
+const filtroOrden = document.querySelector('select[data-testid="select-sort"]');//nos da el valor seleccionado
+
+function soloUnFiltro() {//checa si hemos seleccionado un algo o algo vacio
+  const claveSeleccionada = filtroClave.value;
+  const ordenSeleccionado = filtroOrden.value;
+
+  if (claveSeleccionada) {
+    // si se  selecciona un campo del filtro
+    const datosFiltrados = filterData(data, claveSeleccionada);// lo guarda en esta variable 
+
+    if (ordenSeleccionado) {
+      // Se ha seleccionado un campo de ordenamiento
+      const datosOrdenados = sortData(datosFiltrados, 'name', ordenSeleccionado);// lo guarda en esta variable 
+      renderItems(datosOrdenados);//renderizamos 
+    } else {
+      // No se ha seleccionado un campo de ordenamiento
+      renderItems(datosFiltrados);
+    }
+  } else if (ordenSeleccionado) {
+    // No se ha seleccionado un campo de filtrado, pero se ha seleccionado un campo de ordenamiento
+    const datosOrdenados = sortData(data, 'name', ordenSeleccionado);// lo guarda en esta variable 
+    renderItems(datosOrdenados);//renderizamos 
+  } else {
+    // No se ha seleccionado ni un campo de filtrado ni un campo de ordenamiento
     renderItems(data);
-});
-    const filtronombre = document.querySelector('select[data-testid="select-filter" ]');
-filtronombre.addEventListener("change", function (event) {
-    const Valorseleccionado = event.target.value;
-  const familiasimpson = filterData(data,Valorseleccionado,''); //renderizar
-  renderItems(familiasimpson);
-});
+  }
+}
 
-const ascdesc = document.querySelector('select[data-testid="select-sort"]');
-ascdesc.addEventListener("change", function (event2) {
- const abecetario = event2.target.value;
-
- const datosOrdenadosAsc = filterSort(datos, "name", "asc");
-renderItems(datosOrdenadosAsc);
-
-const datosOrdenadosDesc = filterSort(datos, "name", "desc");
-renderItems(datosOrdenadosDesc);
+filtroClave.addEventListener("change", soloUnFiltro);//nuestra solo un filtro
+filtroOrden.addEventListener("change", soloUnFiltro);//nuestra solo un filtro
 
 
-            
-});
