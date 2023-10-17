cc06b4a1ae6acaee1ed1bf8e9ebf57e0649248a9
DanielaBustamante <bustamantegd@outlook.com>
2023-10-16 18:07:26 -0600

    nofunciona

--- src/main.js
+++ src/main.js
@@ -10,44 +10,58 @@ document.body.appendChild(footer);
 const dataview = document.getElementById('root');
 const datalist = renderItems(data);
 dataview.appendChild(datalist);
+
+function renderizarLista(items) {
+  const lista = document.querySelector('ul[data-testid="item-list"]');
+  lista.innerHTML = '';
+
+  items.forEach(item => {
+    const li = document.createElement('li');
+    const text = document.createTextNode(item.name); // Cambiar 'name' según lo que quieras mostrar
+    li.appendChild(text);
+    lista.appendChild(li);
+  });
+}
+
+// Llama a esta función para renderizar los elementos iniciales
+renderizarLista(data);
+
+// Luego, cuando cambia el filtro o se pulsa el botón 'limpiar', llama a esta función
+// pasándole los datos filtrados o los datos originales para que los renderice
+
+const filtroClave = document.querySelector('select[data-testid="select-filter"]');
+const filtroOrden = document.querySelector('select[data-testid="select-sort"]');
+
+function soloUnFiltro() {
+  const claveSeleccionada = filtroClave.value;
+  const ordenSeleccionado = filtroOrden.value;
+  dataview.innerHTML = '';
+
+  if (claveSeleccionada) {
+    const datosFiltrados = filterData(data, claveSeleccionada);
+
+    if (ordenSeleccionado) {
+      const datosOrdenados = sortData(datosFiltrados, 'name', ordenSeleccionado);
+      renderizarLista(datosOrdenados);
+    } else {
+      renderizarLista(datosFiltrados);
+    }
+  } else if (ordenSeleccionado) {
+    const datosOrdenados = sortData(data, 'name', ordenSeleccionado);
+    renderizarLista(datosOrdenados);
+  } else {
+    renderizarLista(data);
+  }
+}
+
+filtroClave.addEventListener("change", soloUnFiltro);
+filtroOrden.addEventListener("change", soloUnFiltro);
+
 const boton = document.querySelector('button[id="limpiar"]');
 const limpiofiltro1 = document.querySelector('select[data-testid="select-filter"]');
 const limpiofiltro2 = document.querySelector('select[data-testid="select-sort"]');
 boton.addEventListener('click', function (event) {
   limpiofiltro1.selectedIndex = event.target.value;
   limpiofiltro2.selectedIndex = event.target.value;
-  renderItems(data);
+  soloUnFiltro();
 });
-
-const filtroClave = document.querySelector('select[data-testid="select-filter"]');//nos da el valor seleccionado
-const filtroOrden = document.querySelector('select[data-testid="select-sort"]');//nos da el valor seleccionado
-
-function soloUnFiltro() {//checa si hemos seleccionado un algo o algo vacio
-  const claveSeleccionada = filtroClave.value;
-  const ordenSeleccionado = filtroOrden.value;
-  dataview.innerHTML ='';
- 
-  if (claveSeleccionada) {
-    // si se  selecciona un campo del filtro
-    const datosFiltrados = filterData(data, claveSeleccionada);// lo guarda en esta variable 
-
-    if (ordenSeleccionado) {
-      // Se ha seleccionado un campo de ordenamiento
-      const datosOrdenados = sortData(datosFiltrados, 'name', ordenSeleccionado);// lo guarda en esta variable 
-      renderItems(datosOrdenados,dataview);//renderizamos 
-    } else {
-      // No se ha seleccionado un campo de ordenamiento
-      renderItems(datosFiltrados,dataview);
-    }
-  } else if (ordenSeleccionado) {
-    // No se ha seleccionado un campo de filtrado, pero se ha seleccionado un campo de ordenamiento
-    const datosOrdenados = sortData(data, 'name', ordenSeleccionado);// lo guarda en esta variable 
-    renderItems(datosOrdenados,dataview);//renderizamos 
-  } else {
-    // No se ha seleccionado ni un campo de filtrado ni un campo de ordenamiento
-    renderItems(data,dataview);
-  }
-}
-filtroClave.addEventListener("change", soloUnFiltro);//nuestra solo un filtro
-filtroOrden.addEventListener("change", soloUnFiltro);//nuestra solo un filtro
-
