5a717d2e645fc6c3565967dd968f38a93f896070
MarthaMIMS <marthameliton1903@gmail.com>
2023-10-20 17:53:53 -0600

    cambios movil 20102023

--- src/main.js
+++ src/main.js
@@ -40,7 +40,7 @@ function soloUnFiltro() {//checa si hemos seleccionado un algo o algo vacio
 
     console.log(datosFiltrados);
     const informacion_Genero = computeStats(datosFiltrados);
-    parrafo_genero.innerHTML = "info: " +"En esta familia hay: " + informacion_Genero.hombres + " hombres y "+ informacion_Genero.mujeres + " mujeres"
+    parrafo_genero.innerHTML = "Info: " +"En esta familia hay " + "hombres: " + informacion_Genero.hombres + " , "+ " mujeres: " + informacion_Genero.mujeres 
 
     if (ordenSeleccionado) {
       // Se ha seleccionado un campo de ordenamiento
